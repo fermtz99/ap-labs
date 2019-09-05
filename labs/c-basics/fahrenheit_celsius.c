6894c0b410d9a2573fc50f0bb148fb18adf82569
Fernando Martinez <A01630401@itesm.mx>
2019-09-05 18:13:05 -0500

    c-basics lab completed
    Signed-off-by: Fernando Martinez <A01630401@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
-/* print Fahrenheit-Celsius table */
-
-int main()
+int main(int argc, char **argv)
 {
     int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
+    int lower;
+    int upper;
+    int step;
+    if (argc < 2) {
+        printf("You need to send the number of grades to convert\n");
+        printf("How to execute: ./fahrenheit_celsius.o  <number>\n");
+        return 1;
+    }
+    if (argc == 2) {
+	fahr = atoi(argv[1]);
+    	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+    else if (argc == 4) {
+	lower = atoi(argv[1]);
+	upper = atoi(argv[2]);
+	step = atoi(argv[3]);
+    	for (fahr = lower; fahr <= upper; fahr = fahr + step)
+		printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
     return 0;
 }
