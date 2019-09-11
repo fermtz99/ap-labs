7ab74e6fa4374013adf525a722376db3ef1a499d
Fernando Martinez <A01630401@itesm.mx>
2019-09-10 21:19:54 -0500

    Go class activities completed
    Signed-off-by: Fernando Martinez <A01630401@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	newMap := make(map[string]int)
+	
+	for _, i := range strings.Fields(s) {
+		newMap[i] += 1
+	}
+	return newMap
+}
+
 func main() {
+	wc.Test(WordCount)
 }
