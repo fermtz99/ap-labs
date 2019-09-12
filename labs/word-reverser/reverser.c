592ec91507ea34c207b141039fb0710062dbda0e
Fernando Martinez <A01630401@itesm.mx>
2019-09-12 17:15:16 -0500

    Word reverser completed
    Signed-off-by: Fernando Martinez <A01630401@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,32 @@
+// Fernando Martinez A01630401
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+static int n;
+
+void reverse(int lenght, char word[]) {
+	n = 0;
+	int i;
+	char tmp;
+	for(i = 0; i < lenght/2; i++) {
+		tmp = word[i];
+		word[i] = word[lenght - i - 1];
+		word[lenght - i - 1] = tmp;
+	}
+	printf("%s\n", word);
+}
+
+int main(int argc, char **argv){
+	char c;
+	char word[100];
+	n = 0;
+	while ((c = getchar()) != EOF) {
+		word[n++] = c;
+		if(c == '\n') {
+			reverse(n, word);
+			memset(word, 0, 100);
+		}
+	}
+	return 0;
 }
