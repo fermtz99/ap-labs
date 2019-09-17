2b82732e542476dc50c94ec92bf69f91ef42f192
Fernando Martinez <A01630401@itesm.mx>
2019-09-17 16:22:04 -0500

    String Manipulation Library completed
    Signed-off-by: Fernando Martinez <A01630401@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *str);
+char* mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    char* str = argv[1];
+    int len = mystrlen(str);
+    printf("Initial Lenght\t: %d\n", len);
+
+    char* other = argv[2];
+    char* newString = mystradd(str, other);
+    printf("New String\t: %s\n", newString);
+
+    char* toCompare = argv[3];
+    int compared = mystrfind(newString, toCompare);
+    if(compared == 1)
+        printf("Substring was found : yes\n");
+    else
+        printf("Substring was found : no\n");
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,40 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i;
+    for(i = 0; str[i]; i++);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int len1 = mystrlen(origin);
+    int len2 = mystrlen(addition);
+    char *stradded = malloc(len1+len2+1);
+    int i;
+    for(i = 0; i < len1; i++) {
+        stradded[i] = origin[i];
+    }
+    int e;
+    for(e = 0; e < len2; e++) {
+        stradded[i++] = addition[e];
+    }
+    return stradded;
 }
 
 int mystrfind(char *origin, char *substr){
+    int len1 = mystrlen(origin);
+    int len2 = mystrlen(substr);
+    int i;
+    int e;
+    for(i = 0; i < len1; i++) {
+        if(origin[i] == substr[0]) {
+            for(e = 0; e < len2; e++) {
+                if(origin[i+e] != substr[e])
+                    break;
+            }
+            if(e == len2)
+                return 1;
+        }
+    }
     return 0;
 }
