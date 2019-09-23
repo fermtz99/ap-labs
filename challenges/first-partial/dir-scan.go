0975c44069d81c1df576e1682ba3fd3cf0b83018
Fernando Martinez <A01630401@itesm.mx>
2019-09-22 21:45:11 -0500

    First Term Partial finished
    Signed-off-by: Fernando Martinez <A01630401@itesm.mx>

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -3,11 +3,52 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
-
 // scanDir stands for the directory scanning implementation
+
 func scanDir(dir string) error {
-	return nil
+	directNum := 0
+	linksNum := 0
+	devicesNum := 0
+	socketsNum := 0
+	othersNum := 0
+
+	file := func(path string, fileInfo os.FileInfo, e error) error {
+		fType := string(os.FileMode.String(fileInfo.Mode())[0])
+		if fType == "d" {
+			directNum++
+		} else if fType == "L" {
+			linksNum++
+		} else if fType == "D" {
+			devicesNum++
+		} else if fType == "S" {
+			socketsNum++
+		} else {
+			othersNum++
+		}
+		return e
+	}
+	err := filepath.Walk(dir, file)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Steps to run this program:")
+		fmt.Println("1. Build: go build dir-scan.go")
+		fmt.Println("2. Run: sudo ./dir-scan <directory>")
+		os.Exit(1)
+	}
+
+	fmt.Println("Directory Scanner Tool")
+	fmt.Println("+-------------------------+------+")
+	fmt.Println("| Path                    |",dir,"|")
+	fmt.Println("+-------------------------+------+")
+	fmt.Println("| Directories             | ",directNum," |")
+	fmt.Println("| Symbolic Links          | ",linksNum,"  |")
+	fmt.Println("| Devices                 | ",devicesNum,"  |")
+	fmt.Println("| Sockets                 | ",socketsNum,"  |")
+	fmt.Println("| Other files             | ",othersNum,"  |")
+	fmt.Println("+-------------------------+------+")
+	return err
 }
 
 func main() {
--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -1,8 +1,24 @@
+//Fernando Martinez A01630401
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define REPORT_FILE "packages_report.txt"
 
 void analizeLog(char *logFile, char *report);
+
+struct Package {
+    char name[30];
+    char installDate[17];
+    char lastUpdate[17];
+    int updateTimes;
+    char removalDate[17];
+};
+
+static int removedPackages = 0;
+static int upgradedPackages = 0;
 
 int main(int argc, char **argv) {
 
@@ -20,6 +36,180 @@ void analizeLog(char *logFile, char *report) {
     printf("Generating Report from: [%s] log file\n", logFile);
 
     // Implement your solution here.
+    char data[1000000];
+    int arrayIndex = 0;
+    struct Package packages[10000];
+    int filedesc = open(logFile, O_RDONLY);
+    if(filedesc < 0) {
+        printf("File not found\n");
+        return 1;
+    }
+    else {
+        printf("File opened\n");
+        int fileRead = read(filedesc, data, 10000000);
+        if(fileRead < 0) {
+            printf("File not read\n");
+        }
+        else {
+            printf("Reading input file\n");
+            char line[200];
+            int i = 0;
+            int k = 0;
+            while(data[i] != '\0') {
+                line[k] =  data[i];
+                if(data[i] == '\n') {
+                    char date[17];
+                    int j;
+                    for(j = 0; j < 16; j++) {
+                        date[j] = line[j+2];
+                    }
+                    char *action;
 
-    printf("Report is generated at: [%s]\n", report);
+
+                    action = strstr(line, "installed");
+                    if(action != NULL) {
+                        char name[30];
+                        action +=10;
+                        int o = 0;
+                        while(action[o] != ' ') {
+                            name[o] = action[o];
+                            o++;
+                        }
+
+                        //Check if the program exists in the array
+                        int p;
+                        for(p = 0; p < arrayIndex; p++) {
+                            if(strcmp(packages[p].name, name) != 0)
+                                continue;
+                            else
+                               ;
+                        }
+                        if(p == arrayIndex) {
+                            //new entry
+                            strcpy(packages[arrayIndex].name, name);
+                            strcpy(packages[arrayIndex].installDate, date);
+                            strcpy(packages[arrayIndex].lastUpdate, date);
+                            packages[arrayIndex].updateTimes = 0;
+                            strcpy(packages[arrayIndex].removalDate, "-");
+                            arrayIndex++;
+                       }
+                       int t;
+                       for(t = 0; t < 100; t++){
+                           name[t] = '\0';
+                       }
+                    }
+                    else {
+
+
+
+                        action = strstr(line, "upgraded");
+                        if(action != NULL) {
+                            //printf("UPGRADE\n");
+                            char name[30];
+                            action += 9;
+                            int o = 0;
+                            while(action[o] != ' ') {
+                                name[o] = action[o];
+                                o++;
+                            }
+                            int p;
+                            for(p = 0; p < arrayIndex; p++) {
+                                if(strcmp(packages[p].name, name) == 0) {
+                                    if(packages[p].updateTimes == 1)
+                                        upgradedPackages++;
+                                    strcpy(packages[p].lastUpdate, date);
+                                    packages[p].updateTimes++;
+                                    p = arrayIndex;
+                                }
+                            }
+                            int t;
+                            for(t = 0; t < 100; t++){
+                                name[t] = '\0';
+                            }
+                        }
+                        else {
+
+
+
+                            action = strstr(line, "removed");
+                            if(action != NULL) {
+                                //printf("REMOVED");
+                                char name[30];
+                                action += 8;
+                                int o = 0;
+                                while(action[o] != ' ') {
+                                    name[o] = action[o];
+                                    o++;
+                                }
+                                int p;
+                                for(p = 0; p < arrayIndex; p++) {
+                                    if(strcmp(packages[p].name, name) == 0) {
+                                        removedPackages++;
+                                        strcpy(packages[p].removalDate, date);
+                                        p = arrayIndex;
+                                    }
+                                }
+
+                                int t;
+                                for(t = 0; t < 100; t++){
+                                    name[t] = '\0';
+                                }
+                            }
+                        }
+                    }
+                    int m;
+                    for(m = 0; m <400; m++)
+                        line[m] = ' ';
+                    k = 0;
+                }
+                i++;
+                k++;
+            }
+        }
+
+        if(close(filedesc) < 0) {
+            printf("File not closed");
+            return 1;
+        }
+    }
+
+    char finalData[10000];
+    strcat(finalData, "\nPacman Packages Report\n");
+    strcat(finalData, "----------------------\n");
+    char dataBuffer[120];
+    int currentInstalled = arrayIndex - removedPackages;
+    sprintf(dataBuffer, "- Installed packages : %d\n- Removed packages   : %d\n- Upgraded packages  : %d\n- Current installed  : %d\n\n", arrayIndex, removedPackages, upgradedPackages, currentInstalled);
+    strcat(finalData, dataBuffer);
+    strcat(finalData, "List of packages\n");
+    strcat(finalData, "----------------\n");
+    char dataBuffer2[300];
+    //printf("indexArray %d\n", arrayIndex);
+    int h;
+    for(h = 0; h < arrayIndex; h++) {
+        sprintf(dataBuffer2, "- Package name        : %s\n  - Install date      : %s\n  - Last update date  : %s\n  - How many updates  : %d\n  - Removal date      : %s\n", packages[h].name, packages[h].installDate, packages[h].lastUpdate, packages[h].updateTimes, packages[h].removalDate);
+        strcat(finalData, dataBuffer2);
+    }
+    //printf("%s\n", finalData);
+
+    //printf("Report is generated at: [%s]\n", report);
+    int filedesc2 = open(report, O_CREAT | O_WRONLY, 10000);
+    if(filedesc2 < 0) {
+        printf("File not found\n");
+        return 1;
+    }
+    else {
+        printf("File output opened\n");
+        int fileWrite = write(filedesc2, finalData, 10000000);
+        if(fileWrite < 0) {
+            printf("File not written");
+        }
+        else
+            printf("Output file written\n");
+        if(close(filedesc2) < 0) {
+            printf("File not closed");
+            return 1;
+        }
+
+    }
+
 }
