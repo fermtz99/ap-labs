847b9123187dc20ac6075533f090e9783f11bc63
Fernando Martinez <A01630401@itesm.mx>
2019-10-22 18:57:42 -0500

    First part of moitor
    Signed-off-by: Fernando Martinez <A01630401@itesm.mx>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,141 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
 
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+
+#define BLACK 		0
+#define RED		1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+int logFlag = 0;
+
+void textcolor(int attr, int fg, int bg)
+{	char command[13];
+
+	/* Command is the control command to the terminal */
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
+}
+
 int initLogger(char *logType) {
+	if((strcmp(logType, "")) == 0) {
+		// Default
+		logFlag = 0;
+		//printf("Initializing Logger on: stdout\n");
+	}
+	else if((strcmp(logType, "stdout")) == 0) {
+		// Stdout
+		logFlag = 0;
+	}
+	else if((strcmp(logType, "syslog")) == 0) {
+		// Syslog
+		logFlag = 1;
+	}
+	else {
+		logFlag = 0;
+		printf("Not recognized log destination, going to default stdout\n");
+	}
     printf("Initializing Logger on: %s\n", logType);
     return 0;
 }
 
 int infof(const char *format, ...) {
-    return 0;
+	va_list arg;
+	va_start(arg, format);
+	int done;
+	// Normal text color
+	textcolor(RESET, WHITE, BLACK);
+
+	if(logFlag) {
+		openlog("myLog", LOG_NDELAY, LOG_DAEMON);
+		vsyslog(LOG_INFO, format, arg);
+		closelog();
+	}
+	else {
+		done = vfprintf(stdout, format, arg);
+	}
+
+	va_end(arg);
+	return done;
 }
 
 int warnf(const char *format, ...) {
-    return 0;
+	va_list arg;
+	va_start(arg, format);
+        int done;
+	// Warning text color
+        textcolor(RESET, YELLOW, BLACK);
+
+	if(logFlag) {
+		openlog("myLog", LOG_NDELAY, LOG_DAEMON);
+		vsyslog(LOG_WARNING, format, arg);
+		closelog();
+	}
+	else {
+        	done = vfprintf(stdout, format, arg);
+	}
+
+        va_end(arg);
+	textcolor(RESET, WHITE, BLACK);
+        return done;
 }
 
 int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+	va_list arg;
+        va_start(arg, format);
+	int done;
+	// Error text color
+        textcolor(BRIGHT, RED, BLACK);
+
+	if(logFlag) {
+		openlog("myLog", LOG_NDELAY, LOG_DAEMON);
+		vsyslog(LOG_ERR, format, arg);
+		closelog();
+	}
+	else {
+		done = vfprintf(stdout, format, arg);
+	}
+
+        va_end(arg);
+        textcolor(RESET, WHITE, BLACK);
+        return done;
 }
 
+int panicf(const char *format, ...) {
+	va_list arg;
+	va_start(arg, format);
+        int done;
+        // Normal text color
+        textcolor(RESET, WHITE, BLACK);
+
+	if(logFlag) {
+		openlog("myLog", LOG_NDELAY, LOG_DAEMON);
+		vsyslog(LOG_EMERG, format, arg);
+		closelog();
+	}
+	else {
+		printf("PANIC MODE: ");
+		done = vfprintf(stdout, format, arg);
+	}
+
+        va_end(arg);
+        textcolor(RESET, WHITE, BLACK);
+
+	raise(SIGABRT);
+        return -1;
+}
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <ftw.h>
+#include <sys/inotify.h>
 #include "logger.h"
 
-int main(){
+int main(int argc, char** argv){
+	int inotifyFileD;
+	int watch;
+	ssize_t numRead;
+	if(argc == 2) {
+		inotifyFileD = inotify_init();
+		if(inotifyFileD == -1)
+			errorf("iNotify not created\n");
+		watcher = inotify_add_watch(inotifyFileD, argv[1], IN_ALL_EVENTS);
+		if(watch == -1)
+			errorf("Watch not created");
+	}
+	else
+		infof("Usage: ./monitor 'path'");
+
     // Place your magic here
     return 0;
 }
